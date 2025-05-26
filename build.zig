92e0b539db1d6e1c00142a9b5a80cc6378dc3f13
Jeremy Larkin <jeremylarkin@gmail.com>
2025-05-26 13:05:36 -0700

    add build module

--- build.zig
+++ build.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const root_source_file = "src/root.zig";
+
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path(root_source_file),
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+
+    _ = b.addModule("small_array_list", .{
+        .root_source_file = b.path(root_source_file),
+    });
 }
